5ed1c980f0ba88e418855a1070b903cf06b0a700
novita <novinovitaadelia@gmail.com>
2026-09-08 19:46:29 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,11 @@ use App\Models\Prodi;
 use App\Models\Matakuliah;
 use App\Models\Lecturer;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Models\PengajaranMahasiswa;
-=======
-use App\Models\Matakuliah;
-use App\Models\Lecturer;
-use App\Models\Student;
 
->>>>>>> origin/main
+
 
 Route::get('/', function () {
 
